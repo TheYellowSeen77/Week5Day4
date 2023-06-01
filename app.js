acdfb8aaf287cae647331f7b30793f8e0f7bd6f3
Charles Strong <charles.strong3115@stu.mtec.edu>
2023-05-31 18:19:06 -0600

    Week 5 Day 4 Project

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+$(document).ready(function (){
+    $('li:even').css('background-color' , '#ccc')
+});
+$(document).ready(function (){
+    $('li:odd').css('background-color' , '#aaa')
+});
+$('input').val('Charlie');
